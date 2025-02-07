c1b81ea512835edbe6fc798412a8677f8679258c
lubie-koty <art.zelazko@gmail.com>
2025-02-07 21:55:45 +0100

    update configa

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+}
